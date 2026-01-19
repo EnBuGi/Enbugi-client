cfcb29d85e1755c975629a0f67ececebccd43f6b
vvineey <vvin506@naver.com>
2026-01-19 18:55:37 +0900

    Chore: 스토리북 기본 설정 파일 추가

---
+++ .storybook/preview.tsx
@@ -0,0 +1,39 @@
+import type { Preview } from '@storybook/nextjs-vite';
+import '../src/app/globals.css';
+import React from 'react';
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#09090b',
+        },
+        {
+          name: 'light',
+          value: '#ffffff',
+        },
+      ],
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      test: 'todo',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Story />
+    ),
+  ],
+};
+
+export default preview;
+
