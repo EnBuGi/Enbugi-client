53a20d3b7229897c52359cdfd1ab9d6ccbe86f51
vvineey <vvin506@naver.com>
2026-02-15 03:56:44 +0900

    [Fix] max-w-l 오타 수정
    - max-w-6xl로 수정함

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import ProjectList from "@/features/mentee-projects/ui/ProjectList";
 
 export default function ProjectsPage() {
     return (
-        <div className="max-w-l mx-auto py-20">
+        <div className="max-w-6xl mx-auto py-20">
             <ProjectList />
         </div>
     );
